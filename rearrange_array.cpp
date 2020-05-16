e074e779ed16c67e8cea4c0e910ebb0506e518c2
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-05-16 08:38:52 +0530

    Create rearrange_array.cpp

---
+++ rearrange_array.cpp
@@ -0,0 +1,7 @@
+void Solution::arrange(vector<int> &A) {
+    int n = A.size();
+    for(int i=0; i<n; i++)
+        A[i] = n * (A[A[i]]%n) + A[i];
+    for(int i=0; i<n; i++)
+        A[i] = A[i] / n;
+}
