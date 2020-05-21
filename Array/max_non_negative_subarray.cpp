2982b39f3e63522af30deb1495a65f0119341f92
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-05-21 17:31:48 +0530

    Update max_non_negative_subarray.cpp

--- Array/max_non_negative_subarray.cpp
+++ Array/max_non_negative_subarray.cpp
@@ -23,3 +23,42 @@ vector<int> Solution::maxset(vector<int> &A) {
     }
     return ans;
 }
+
+//Optimized code
+vector<int> Solution::maxset(vector<int> &A) {
+    int n = A.size();
+    long long int sum = 0;
+    long long int max_sum = -1;
+    vector<int> ans;
+    vector<int> tmp;
+    int start = -1;
+    int end = -1;
+    int sz = -1;
+    int s=-1, e=-1;
+    for(int i=0; i<n; i++) {
+        if(A[i] >= 0) {
+            sum += A[i];
+            if(start == -1)
+                start = i;
+            end = i;
+        }
+        else {
+            if(max_sum < sum || (start!=-1 && max_sum==sum && sz<end-start+1)) {
+                s = start;
+                e = end;
+                sz = start-end+1;
+                max_sum = sum;
+            }
+            start = -1;
+            end = -1;
+            sum = 0;
+        }
+    }
+    if(max_sum < sum){
+        s = start;
+        e = end;
+    }
+    for(int i=s; i<=e&&s!=-1; i++)
+        ans.push_back(A[i]);
+    return ans;
+}
