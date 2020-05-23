4663d8a69229c8250476d95c739f573b76b190c7
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-05-23 18:04:24 +0530

    Update maximum_unsorted_subarray.cpp

--- Array/maximum_unsorted_subarray.cpp
+++ Array/maximum_unsorted_subarray.cpp
@@ -1,4 +1,4 @@
-//)(nlogn)
+//O(nlogn)
 
 vector<int> Solution::subUnsort(vector<int> &A) {
     vector<int> tmp(A);
@@ -20,4 +20,50 @@ vector<int> Solution::subUnsort(vector<int> &A) {
         return vector<int>{-1};
     else
         return vector<int>{s, e};
+}
+
+//O(N) solution
+vector<int> Solution::subUnsort(vector<int> &A) {
+    int n = A.size();
+    int i=0;
+    while(i<n-1) {
+        if(A[i] <= A[i+1])
+            i++;
+        else 
+            break;
+    }
+    if(i == n-1)
+        return vector<int>{-1};
+    int j=n-1;
+    while(j>=0) {
+        if(A[j] >= A[j-1])
+            j--;
+        else
+            break;
+    }
+    if(i > j)
+        swap(i, j);
+    int max_sub = INT_MIN;
+    int min_sub = INT_MAX;
+    int start = i;
+    int end = j;
+    // cout << "i: " << i << endl;
+    // cout << "j: " << j << endl;
+    for(int p=i; p<=j; p++) {
+        max_sub = max(max_sub, A[p]);
+        min_sub = min(min_sub, A[p]);
+    }
+    for(int p=0; p<i; p++) {
+        if(A[p] > min_sub){
+            start = p;
+            break;
+        }
+    }
+    for(int p=n-1; p>j; p--) {
+        if(A[p] < max_sub) {
+            end = p;
+            break;
+        }
+    }
+    return vector<int>{start, end};
 }
