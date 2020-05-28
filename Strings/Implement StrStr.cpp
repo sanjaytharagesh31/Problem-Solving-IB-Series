c2b47ed413320ffb330e3c04c048c8c05e72d42b
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-05-28 21:54:57 +0530

    Update Implement StrStr.cpp

--- Strings/Implement StrStr.cpp
+++ Strings/Implement StrStr.cpp
@@ -1,3 +1,47 @@
+//KMP Algorithm 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string txt = "abcdabcd"; 
+    string pat = "cda"; 
+    int m = txt.length();
+    int n = pat.length();
+    vector<int> arr(n);
+    arr[0] = 0;
+    int i = 0;
+    int j = 1;
+    while(j<n) {
+        if(pat[i] == pat[j])
+            arr[j++] = i++;
+        else {
+            if(i > 0)
+                i = arr[i-1];
+            else
+                arr[j++] = 0;
+        }
+    }
+    i=0;
+    j=0;
+    while(i<m) {
+        if(txt[i] == pat[j]) {
+            i++;
+            j++;
+            if(j == n) {
+                cout << i - n << endl;
+                j = arr[j-1];
+            }
+        }
+        else {
+            if(j > 0)
+                j = arr[j-1];
+            else
+                i++;
+        }
+    }
+}
+
+//TODO: Fix bugs in Aux array
 int Solution::strStr(const string A, const string B) {
     int m = A.length();
     int n = B.length();
