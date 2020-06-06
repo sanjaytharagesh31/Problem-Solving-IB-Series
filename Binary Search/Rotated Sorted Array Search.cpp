8a34350c0d195826eedce6f672e7ddd65828195b
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-06 17:30:39 +0530

    Update Rotated Sorted Array Search.cpp

--- Binary Search/Rotated Sorted Array Search.cpp
+++ Binary Search/Rotated Sorted Array Search.cpp
@@ -1,4 +1,4 @@
-
+//2 Pass Approach
 int Solution::search(const vector<int> &A, int B) {
     int l = 0;
     int h = A.size()-1;
@@ -31,5 +31,35 @@ int Solution::search(const vector<int> &A, int B) {
         else
             h = m-1;
     }
+    return -1;
+}
+
+//1 Pass Approach
+
+int Solution::search(const vector<int> &A, int B) {
+    int l = 0;
+    int h = A.size()-1;
+        while(l <= h) {
+            int m = (l+h)/2;
+            if(A[m] == B) {
+                return m;
+            }
+            else if(A[m] > A[0]) {
+                if(B>=A[0] && B<=A[m]) {
+                    h = m-1;
+                }
+                else {
+                    l = m+1;
+                }
+            }
+            else {
+                if(B>=A[m] && B<=A[A.size()-1]) {
+                    l = m+1;
+                }
+                else {
+                    h = m-1;
+                }
+            }
+        }
     return -1;
 }
