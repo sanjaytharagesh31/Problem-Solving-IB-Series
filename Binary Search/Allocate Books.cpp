386e69d562bc24f1bc9809583e88ba47a4dad422
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-06 22:30:52 +0530

    Update Allocate Books.cpp

--- Binary Search/Allocate Books.cpp
+++ Binary Search/Allocate Books.cpp
@@ -1,3 +1,43 @@
+//Efficient Solution
+int Solution::books(vector<int> &A, int B) {
+    if(A.size() < B)
+        return -1;
+    int ans = INT_MAX;
+    int mx_pg = 0;
+    int mn_pg = 0;
+    for(int x : A)
+        mx_pg += x;
+    while(mn_pg <= mx_pg) {
+        int m_pg = (mn_pg+mx_pg)/2;
+        int cnt = 0;
+        int pg = 0;
+        for(int i=0; i<A.size(); i++) {
+            if(A[i] > m_pg) {
+                cnt = INT_MAX;
+                break;
+            }
+            pg += A[i];
+            if(pg > m_pg) {
+                cnt++;
+                pg = A[i];
+            }
+        }
+        cnt++;
+        if(cnt <= B) {
+            ans = min(ans, m_pg);
+            mx_pg = m_pg-1;
+            // break;
+        }
+        else if(cnt < B) {
+            mx_pg = m_pg-1;
+        }
+        else {
+            mn_pg = m_pg+1;
+        }
+    }
+    return ans;
+}
+
 //Brute Force (TLE)
 int solve(vector<int> &arr, int p, int n) {
     if(n == 1) {
