dc524accb708dc15f482a5c0bda8111fbb5d44d4
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-08 09:42:42 +0530

    update eff algo

--- Bit Manipulation/Number of 1 Bits.cpp
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -1,9 +1,20 @@
+//O(sizeof(int))
 int Solution::numSetBits(unsigned int A) {
     int ans = 0;
     for(int i=0; i<=31; i++) {
         if(A>>i & 1 == 1) {
             ans++;
         }
+    }
+    return ans;
+}
+
+//O(Numebr of 1 bits)
+int Solution::numSetBits(unsigned int A) {
+    int ans = 0;
+    while(A > 0) {
+        ans++;
+        A = A&(A-1);
     }
     return ans;
 }
