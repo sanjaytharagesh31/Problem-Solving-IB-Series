edacb97a5ae7908fda6ea3bf2dfdb2bce3087ac5
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-09 15:20:32 +0530

    Update Merge Two Sorted Lists II.cpp

--- Two Pointers/Merge Two Sorted Lists II.cpp
+++ Two Pointers/Merge Two Sorted Lists II.cpp
@@ -1,3 +1,26 @@
+//O(1) Space
+void Solution::merge(vector<int> &A, vector<int> &B) {
+    int m = A.size();
+    int n = B.size();
+    int i=m-1;
+    int j=n-1;
+    A.resize(m+n);
+    int ind = m+n-1;
+    while(j >= 0) {
+        if(i>=0 && B[j] < A[i]) {
+            A[ind] = A[i];
+            i--;
+            ind--;
+        }
+        else {
+            A[ind] = B[j];
+            ind--;
+            j--;
+        }
+    }
+}
+
+//O(M+N) space
 void Solution::merge(vector<int> &A, vector<int> &B) {
     int m = A.size();
     int n = B.size();
