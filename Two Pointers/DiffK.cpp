7f6f15edee76ad91f796a5c7a4d72d0cb36942bd
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-11 09:47:15 +0530

    Update DiffK.cpp

--- Two Pointers/DiffK.cpp
+++ Two Pointers/DiffK.cpp
@@ -26,3 +26,20 @@ int Solution::diffPossible(vector<int> &A, int B) {
     }
     return 0;
 }
+
+//O(N) Time
+int Solution::diffPossible(vector<int> &A, int B) {
+    int i=0;
+    int j=1;
+    while(i<A.size() && j<A.size()) {
+        if(A[j]-A[i] == B)
+            return 1;
+        else if(A[j]-A[i] > B) {
+            i++;
+        }
+        else {
+            j++;
+        }
+    }
+    return 0;
+}
