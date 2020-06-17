3a509f66236407424d76cbbaa7d22964aecfd5ac
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-17 13:13:21 +0530

    Update Min Stack.cpp

--- Stacks and Queues/Min Stack.cpp
+++ Stacks and Queues/Min Stack.cpp
@@ -1,3 +1,49 @@
+//O(N+X) Space solution, X - number of times minimum changes
+stack<int> s;
+stack<int> mn;
+
+MinStack::MinStack() {
+    s = stack<int>();
+    mn = stack<int>();
+}
+
+void MinStack::push(int x) {
+    s.push(x);
+    if(mn.empty()) {
+        mn.push(x);
+    }
+    else {
+        if(mn.top() >= x)
+            mn.push(x);
+    }
+}
+
+void MinStack::pop() {
+    if(!s.empty()) {
+        if(mn.top() == s.top())
+            mn.pop();
+        s.pop();
+    }
+}
+
+int MinStack::top() {
+    if(s.empty())
+        return -1;
+    else {
+        return s.top();
+    }
+}
+
+int MinStack::getMin() {
+    if(mn.empty())
+        return -1;
+    return mn.top();
+}
+
+
+
+//O(N+N) Space solution
+
 stack<pair<int, int>> s;
 
 MinStack::MinStack() {
