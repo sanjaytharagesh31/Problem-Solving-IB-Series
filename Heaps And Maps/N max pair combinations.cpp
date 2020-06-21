f8918ac8ef4f174b7a55eb58527c10a5ddd12732
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-22 00:17:45 +0530

    update optimal code

--- Heaps And Maps/N max pair combinations.cpp
+++ Heaps And Maps/N max pair combinations.cpp
@@ -1,3 +1,38 @@
+//O(NlogN) time complexity
+
+vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
+    
+    priority_queue<pair<int, vector<int>>> pq;
+    map<vector<int>, int> mp;
+    vector<int> ans;
+    
+    int n = A.size();
+    sort(A.begin(), A.end(), greater<int>());
+    sort(B.begin(), B.end(), greater<int>());
+    
+    pq.push(make_pair(A[0]+B[0], vector<int>{0, 0}));
+    mp[{0, 0}] = 1;
+    
+    while(ans.size() < n) {
+        pair<int, vector<int>> tmp = pq.top(); pq.pop();
+        ans.push_back(tmp.first);
+        int i = tmp.second[0];
+        int j = tmp.second[1];
+        if(mp[{i+1, j}] == 0) {
+            pq.push(make_pair(A[i+1]+B[j], vector<int>{i+1, j}));
+            mp[{i+1, j}] = 1;
+        }
+        if(mp[{i, j+1}] == 0) {
+            pq.push(make_pair(A[i]+B[j+1], vector<int>{i, j+1}));
+            mp[{i, j+1}] = 1;
+        }
+        
+    }
+    
+    return ans;
+}
+
+
 //TLE Solution
 
 vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
