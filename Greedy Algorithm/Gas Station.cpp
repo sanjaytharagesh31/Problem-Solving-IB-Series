589418f2dccf1acef322a96669c6519d953c3616
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-06-27 14:04:34 +0530

    Update Gas Station.cpp

--- Greedy Algorithm/Gas Station.cpp
+++ Greedy Algorithm/Gas Station.cpp
@@ -1,3 +1,24 @@
+//O(N) Time solution
+
+int Solution::canCompleteCircuit(const vector<int> &A, const vector<int> &B) {
+    int n = A.size();
+    int start = 0;
+    int end = 1;
+    int cur = A[0] - B[0];
+    
+    while(end != start || cur < 0) {
+        while(cur < 0 && start != end) {
+            cur = cur - A[start] + B[start];
+            start = (start + 1) % n;
+            if(start == 0)
+                return -1;
+        }
+        cur += A[end] - B[end];
+        end = (end + 1) % n;
+    }
+    return start;
+}
+
 //O(N*N) TIme solution
 
 int Solution::canCompleteCircuit(const vector<int> &A, const vector<int> &B) {
