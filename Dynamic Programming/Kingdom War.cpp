e6236a3999efd3bc7feeff513b13734ccd4b0ea2
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-07-01 09:36:31 +0530

    fix var error

--- Dynamic Programming/Kingdom War.cpp
+++ Dynamic Programming/Kingdom War.cpp
@@ -5,7 +5,7 @@ int Solution::solve(vector<vector<int> > &A) {
     int ans = INT_MIN;
     for(int i=r-1; i>=0; i--) {
         for(int j=c-1; j>=0; j--) {
-            dp[i][j] = A[i][j]+arr[i+1][j]-arr[i+1][j+1]+arr[i][j+1];
+            arr[i][j] = A[i][j]+arr[i+1][j]-arr[i+1][j+1]+arr[i][j+1];
             ans = max(ans, arr[i][j]);
         }
     }
